6275db9463acdfe8543049acac077819c332ad43
ALUNO <guiparadiso2006@gmail.com>
2025-10-21 08:59:24 -0300

    "Prática 1 – Parte 2"

---
+++ app/src/main/java/com/example/weatherapp_marina/LoginActivity.kt
@@ -0,0 +1,103 @@
+package com.example.weatherapp_marina
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.LocalActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.weatherapp_marina.ui.theme.WeatherApp_MarinaTheme
+
+class LoginActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            WeatherApp_MarinaTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                        LoginPage(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun LoginPage(modifier: Modifier = Modifier) {
+    var email by rememberSaveable { mutableStateOf("") }
+    var password by rememberSaveable { mutableStateOf("") }
+    val activity = LocalActivity.current as Activity
+    Column(
+        modifier = modifier.padding(16.dp).fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = CenterHorizontally
+    ) {
+        Text(
+            text = "Bem-vindo/a!",
+            fontSize = 24.sp
+        )
+        OutlinedTextField(
+            value = email,
+            label = { Text(text = "Digite seu e-mail") },
+            modifier = modifier.fillMaxWidth(fraction = 0.9f),
+            onValueChange = { email = it }
+        )
+
+        Spacer(modifier = modifier.size(24.dp))
+
+        OutlinedTextField(
+            value = password,
+            label = { Text(text = "Digite sua senha") },
+            modifier = modifier.fillMaxWidth(fraction = 0.9f),
+            onValueChange = { password = it },
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        Spacer(modifier = modifier.size(24.dp))
+
+        Row(modifier = modifier) {
+            Button( onClick = {
+
+                Toast.makeText(activity, "Login OK!", Toast.LENGTH_LONG).show()
+
+            } ) {
+                Text("Login")
+            }
+
+            Spacer(modifier = modifier.size(24.dp))
+
+            Button(
+                onClick = { email = ""; password = "" },
+                enabled = email.isNotEmpty() && password.isNotEmpty()
+            ) {
+                Text("Limpar")
+            }
+        }
+    }
+}
