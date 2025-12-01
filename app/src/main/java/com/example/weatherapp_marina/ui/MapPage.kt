8e25e92d1de855934dd361bf6801ff2724412b83
mcdssd <mcs56@discente.ifpe.edu.br>
2025-12-01 18:07:22 -0300

    [Pratica 4. Parte 4]: Adicionei a cidade marcada na lista de favoritos.

--- app/src/main/java/com/example/weatherapp_marina/ui/MapPage.kt
+++ app/src/main/java/com/example/weatherapp_marina/ui/MapPage.kt
@@ -1,36 +1,99 @@
 package com.example.weatherapp_marina.ui
 
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
 import com.example.weatherapp_marina.model.MainViewModel
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.MapUiSettings
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
 
+@SuppressLint("UnrememberedMutableState")
 @Composable
 fun MapPage(modifier: Modifier = Modifier,
             viewModel: MainViewModel
 ) {
-    Column(
-        modifier = modifier.fillMaxSize()
-            .background(Color.Companion.Gray)
-            .wrapContentSize(Alignment.Companion.Center)
-    ) {
-        Text(
-            text = "Mapa",
-            fontWeight = FontWeight.Companion.Bold,
-            color = Color.Companion.White,
-            modifier = modifier.align(Alignment.Companion.CenterHorizontally),
-            textAlign = TextAlign.Companion.Center,
-            fontSize = 20.sp
+
+
+    val recife = LatLng(-8.05, -34.9)
+    val caruaru = LatLng(-8.27, -35.98)
+    val joaopessoa = LatLng(-7.12, -34.84)
+    val garanhuns= LatLng(-8.89, -36.49)
+
+    val camPosState = rememberCameraPositionState ()
+
+    val context = LocalContext.current
+    val hasLocationPermission by remember {
+        mutableStateOf(
+            ContextCompat.checkSelfPermission(context,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) ==
+                    PackageManager.PERMISSION_GRANTED
         )
+    }
+
+    GoogleMap (modifier = Modifier.fillMaxSize(),
+        properties = MapProperties(isMyLocationEnabled = hasLocationPermission),
+        uiSettings = MapUiSettings(myLocationButtonEnabled = true),
+        cameraPositionState = camPosState,
+        onMapClick = {
+        viewModel.add("Cidade@${it.latitude}:${it.longitude}", location = it) }
+    ) {
+        viewModel.cities.forEach {
+            if (it.location != null) {
+                Marker( state = MarkerState(position = it.location),
+                    title = it.name, snippet = "${it.location}")
+            }
+        }
+
+        Marker(
+            state = MarkerState(position = recife),
+            title = "Recife",
+            snippet = "Marcador em Recife",
+            icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)
+        )
+
+        Marker(
+            state = MarkerState(position = caruaru),
+            title = "Caruaru",
+            snippet = "Marcador em Caruaru",
+            icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)
+        )
+
+        Marker(
+            state = MarkerState(position = joaopessoa),
+            title = "João Pessoa",
+            snippet = "Marcador em João Pessoa",
+            icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)
+        )
+
+        Marker(
+            state = MarkerState(position = garanhuns),
+            title = "Garanhuns",
+            snippet = "Marcador em Garanhuns",
+            icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)
+        )
+
     }
 }
