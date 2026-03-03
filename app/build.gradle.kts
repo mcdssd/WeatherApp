30f3d7b2fbb2988b2e37d5b66a6fae272b0e1521
mcdssd <mcs56@discente.ifpe.edu.br>
2026-03-03 08:35:28 -0300

    Pratica 9. Parte 1: Substituindo Icon por AsynImage

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,8 @@ dependencies {
     implementation ("com.squareup.retrofit2:retrofit:3.0.0")
     implementation ("com.squareup.retrofit2:converter-gson:3.0.0")
 
+    implementation("io.coil-kt:coil-compose:2.7.0") // coil
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
